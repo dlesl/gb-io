d250e6e21c3b8261ecb606ee9cf9f4debc03e377
David <dlesl@users.noreply.github.com>
2019-03-09 18:50:10 +0100

    benchmark `revcomp` and `set_origin`

--- benches/parse.rs
+++ benches/parse.rs
@@ -2,20 +2,28 @@
 extern crate bencher;
 extern crate gb_io;
 
-
 use bencher::Bencher;
 use gb_io::reader::{parse_slice, SeqReader};
 
-
 const ECOLI: &[u8] = include_bytes!("../tests/mg1655.gb");
 
-fn bench_ecoli_slice(b: &mut Bencher) {
+fn ecoli_slice(b: &mut Bencher) {
     b.iter(|| parse_slice(ECOLI).unwrap());
 }
 
-fn bench_ecoli_streaming(b: &mut Bencher) {
+fn ecoli_streaming(b: &mut Bencher) {
     b.iter(|| SeqReader::new(ECOLI).next().unwrap().unwrap());
 }
 
-benchmark_group!(benches, bench_ecoli_slice, bench_ecoli_streaming);
+fn ecoli_revcomp(b: &mut Bencher) {
+    let ec = SeqReader::new(ECOLI).next().unwrap().unwrap();
+    b.iter(|| ec.revcomp());
+}
+
+fn ecoli_set_origin(b: &mut Bencher) {
+    let ec = SeqReader::new(ECOLI).next().unwrap().unwrap();
+    b.iter(|| ec.set_origin(1_000_000));
+}
+
+benchmark_group!(benches, ecoli_slice, ecoli_streaming, ecoli_revcomp, ecoli_set_origin);
 benchmark_main!(benches);
